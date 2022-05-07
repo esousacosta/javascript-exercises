9f56497dcf53d9bc3e5794192f39ecba137a42c9
esousacosta <victoremanoel23@gmail.com>
2022-05-07 15:48:08 +0200

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(iFirstNumber, iSecondNumber) {
+    if (typeof iFirstNumber !== 'number' || typeof iSecondNumber !== 'number' || iSecondNumber < 0 || iFirstNumber < 0)
+    {
+        return 'ERROR';
+    }
+    
+    let aSmallerNumber;
+    let aLargerNumber;
+    
+    if (iFirstNumber < iSecondNumber)
+    {
+        aSmallerNumber = iFirstNumber;
+        aLargerNumber = iSecondNumber;
+    }
+    else
+    {
+        aLargerNumber = iFirstNumber;
+        aSmallerNumber = iSecondNumber;
+    }
+    
+    let aSum = 0;
+    for (let i = aSmallerNumber; i <= aLargerNumber; i++)
+    {
+        aSum += i;
+    }
 
+    return aSum;
 };
 
 // Do not edit below this line
