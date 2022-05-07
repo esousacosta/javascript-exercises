db6ac2e7b9c268c5a599d2e6d4bcab046ae6ef25
esousacosta <victoremanoel23@gmail.com>
2022-05-07 16:31:50 +0200

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(iFirstNumber, iSecondNumber) {
+  if (typeof iFirstNumber !== 'number' || typeof iSecondNumber !== 'number')
+  {
+    return 'ERROR';
+  }
+  return iFirstNumber + iSecondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(iFirstNumber, iSecondNumber) {
+  if (typeof iFirstNumber !== 'number' || typeof iSecondNumber !== 'number')
+  {
+    return 'ERROR';
+  }
+  return iFirstNumber - iSecondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(iArray) {
+  return iArray.reduce((aPrecedingResult, aNextNumber) => aPrecedingResult + aNextNumber, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(iArray) {
+  return iArray.reduce((aPrecedingResult, aNextNumber) => aPrecedingResult * aNextNumber, 1);
 };
 
-const power = function() {
-	
+const power = function(iBaseNumber, iExponentNumber) {
+  if (typeof iBaseNumber !== 'number' || typeof iExponentNumber !== 'number')
+  {
+    return 'ERROR';
+  }
+  return Math.pow(iBaseNumber, iExponentNumber);
 };
 
-const factorial = function() {
-	
+const factorial = function(iNumber) {
+  if (typeof iNumber !== 'number')
+  {
+    return 'ERROR';
+  }
+  if (iNumber === 0)
+  {
+    return 1;
+  }
+  let aNumberFactorial = 1;
+  while (iNumber > 0)
+  {
+    aNumberFactorial *= iNumber;
+    iNumber--;
+  }
+  return aNumberFactorial;
 };
 
 // Do not edit below this line
