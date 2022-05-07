54ceff2ccb1a77fe911dd850253042bd7acdbb9d
esousacosta <victoremanoel23@gmail.com>
2022-05-07 15:32:15 +0200

    Finish exercise number 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-
+/**
+ * @param {Array} iOriginalArray;
+ */
+const removeFromArray = function(iOriginalArray, iElementsToRemove) {
+    let aIndexOfElement = -1;
+    let aArgsArray = Array.from(arguments);
+    aArgsArray = aArgsArray.slice(1);
+    // This is to create a copy of the original array - needed due to Javascript's
+    // pass by reference + value system.
+    let aModifiedArray = iOriginalArray.slice(0, iOriginalArray.length);
+    for (const element of aArgsArray)
+    {
+        aIndexOfElement = aModifiedArray.indexOf(element) === -1 ? null : aModifiedArray.indexOf(element);
+        if (aIndexOfElement !== -1 && aIndexOfElement !== null)
+        {
+            aModifiedArray.splice(aIndexOfElement, 1);
+        }
+    }
+    return aModifiedArray;
 };
 
 // Do not edit below this line
