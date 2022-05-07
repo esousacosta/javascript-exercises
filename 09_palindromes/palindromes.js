24110900bb5a9473d29269b43932aeddd24c5696
esousacosta <victoremanoel23@gmail.com>
2022-05-07 19:39:02 +0200

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (iPossiblePalindrome) {
+    const aStrippedString = iPossiblePalindrome.replace(/[^0-9A-Za-z]/gi, '');
+    const aPossiblePalindromeArray = aStrippedString.split("");
+    const aRevertedPossiblePalindromeArray = aPossiblePalindromeArray.slice().reverse();
+    let aPalindrome = true;
+    for (let i = 0; i < aPossiblePalindromeArray.length; i++)
+    {
+        if (aPossiblePalindromeArray[i].toUpperCase() !== aRevertedPossiblePalindromeArray[i].toUpperCase())
+        {
+            aPalindrome = false;
+            break;
+        }
+    }
+    return aPalindrome;
 };
 
 // Do not edit below this line
