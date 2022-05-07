1dd77ff4e4206557b61023af72e6e1db846da154
esousacosta <victoremanoel23@gmail.com>
2022-05-07 20:46:51 +0200

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+/**
+ * 
+ * @param {Array} iBooksArray my dictionnary of books and titles
+ */
+const getTheTitles = function(iBooksArray) {
+    const aBookTitlesList = iBooksArray.map((element) => {return element['title'];});
+    return aBookTitlesList;
 };
 
 // Do not edit below this line
