91fa1fd4c2bc193059ca702887a77687be7859f4
esousacosta <victoremanoel23@gmail.com>
2022-05-07 16:04:18 +0200

    Finish exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(iYear) {
+    if (iYear % 4 === 0)
+    {
+        if (iYear % 100 === 0 && !(iYear % 400 === 0))
+        {
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
