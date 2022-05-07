123213070f22878b043144863996c99c09505258
esousacosta <victoremanoel23@gmail.com>
2022-05-07 16:10:31 +0200

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(iTemperatureInFarenheit) {
+  const aConvertedTemperature = (iTemperatureInFarenheit - 32) * (5 / 9);
+  const aTruncatedConvertedTemperature = parseFloat(aConvertedTemperature.toFixed(1));
+  return aTruncatedConvertedTemperature;
 };
 
-const ctof = function() {
-
+const ctof = function(iTemperatureInCelsius) {
+  const aConvertedTemperature = (iTemperatureInCelsius * (9 / 5)) + 32;
+  const aTruncatedConvertedTemperature = parseFloat(aConvertedTemperature.toFixed(1));
+  return aTruncatedConvertedTemperature;
 };
 
 // Do not edit below this line
