c7c3419bdcf80f74b5f09f1db38c1128a0dd8672
esousacosta <victoremanoel23@gmail.com>
2022-05-07 12:34:39 +0200

    Finished exercises number 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+'use strict'
+/*
+ * @param {array} aStringArray the array to modify the string;
+ * @param {string} aReturnString the reversed string to be returned;
+ */
 
+const reverseString = function(iString) {
+    if (iString === '')
+    {
+        return '';
+    }
+    let aStringCharsArray = iString.split("");
+    let aReverseString;
+    
+    aStringCharsArray = aStringCharsArray.reverse();
+    aReverseString = aStringCharsArray.join("");
+    
+    return aReverseString;
 };
 
 // Do not edit below this line
