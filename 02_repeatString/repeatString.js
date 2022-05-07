b331cc133a0a953438de3d5e1ac0d9e179873b7e
esousacosta <victoremanoel23@gmail.com>
2022-05-07 12:05:23 +0200

    Finish exercises number 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(iString, iNumOfRepetitions) {
+    let aReturnedString = "";
+    if (iNumOfRepetitions < 0)
+    {
+        return "ERROR";
+    }
+    for (let i = 0; i < iNumOfRepetitions; i++)
+    {
+        aReturnedString += iString;
+    }
+    return aReturnedString;
 };
 
 // Do not edit below this line
