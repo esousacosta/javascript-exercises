e61cef9d67003912a6acf9fbf67900ac2d2ea0a5
esousacosta <victoremanoel23@gmail.com>
2022-05-07 20:26:02 +0200

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(iFibonacciPosition) {
+    if (typeof iFibonacciPosition === 'string')
+    {
+        iFibonacciPosition = parseInt(iFibonacciPosition);
+    }
+    if (iFibonacciPosition === 1 || iFibonacciPosition === 0)
+    {
+        return iFibonacciPosition;
+    }
+    else if (iFibonacciPosition < 0)
+    {
+        return "OOPS";
+    }
+    return fibonacci(iFibonacciPosition - 1) + fibonacci(iFibonacciPosition - 2);
 };
 
 // Do not edit below this line
